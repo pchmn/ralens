fec26d73b1251ce0e05b773a5aed2e86ca266ef1
pchmn <pchmn.dev@gmail.com>
2023-08-27 18:39:04 +0200

    db: create updatedAt trigger

---
+++ nhost/migrations/default/1692723963340_create_trigger_updatedAt/down.sql
@@ -0,0 +1,23 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION set_updatedAt() RETURNS trigger AS
+-- $$
+-- BEGIN
+--   NEW."updatedAt" := now();
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE 'plpgsql';
+--
+-- DO $$
+-- DECLARE
+--     t text;
+-- BEGIN
+--     FOR t IN
+--         SELECT table_name FROM information_schema.columns WHERE column_name = 'updatedAt'
+--     LOOP
+--         EXECUTE format('CREATE OR REPLACE TRIGGER set_%I_updated_at
+--                     BEFORE UPDATE ON %I
+--                     FOR EACH ROW EXECUTE PROCEDURE set_updatedAt()', t,t);
+--     END loop;
+-- END;
+-- $$ language 'plpgsql';
