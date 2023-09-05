2c0eec1943f15b099c345c6dd2c29eaaeaca400c
pchmn <pchmn.dev@gmail.com>
2023-09-05 18:08:15 +0200

    refactor(mobile): remove expo sensors dependency

--- apps/mobile/src/app/camera/Camera/useCamera.ts
+++ apps/mobile/src/app/camera/Camera/useCamera.ts
@@ -4,7 +4,6 @@ import { gravity, SensorTypes, setUpdateIntervalForType } from 'react-native-sen
 import { Camera, CameraDevice, CameraDeviceFormat, sortFormats, useCameraDevices } from 'react-native-vision-camera';
 
 setUpdateIntervalForType(SensorTypes.gravity, 1000);
-// DeviceMotion.setUpdateInterval(1000);
 
 export function useCamera(ratio: '16:9' | '4:3' = '16:9') {
   const devices = useCameraDevices();
@@ -12,15 +11,6 @@ export function useCamera(ratio: '16:9' | '4:3' = '16:9') {
   const [orientation, setOrientation] = useState<
     'portrait' | 'portraitUpsideDown' | 'landscapeLeft' | 'landscapeRight'
   >('portrait');
-
-  useEffect(() => {
-    // DeviceMotion.addListener(({ orientation, rotation }) => {
-    //   console.log('rotationnnnnn', orientation, calculateOrientation(rotation.gamma, rotation.beta));
-    // });
-    // return () => {
-    //   DeviceMotion.removeAllListeners();
-    // };
-  }, []);
 
   useEffect(() => {
     const subscription = gravity.subscribe(({ x, y }) => {
@@ -52,7 +42,6 @@ export function useCamera(ratio: '16:9' | '4:3' = '16:9') {
 
         return {
           ...acc,
-          // use 'sortFormats' from 'react-native-vision-camera' to get the best resolutions first
           [`photo-${photoRatio}`]: [...(acc[`photo-${photoRatio}`] || []), format].sort(sortFormats),
           [`video-${videoRatio}`]: [...(acc[`video-${videoRatio}`] || []), format].sort(sortFormats),
         };
@@ -94,47 +83,6 @@ export function useCamera(ratio: '16:9' | '4:3' = '16:9') {
   return { device, toggleDevice, ref, takePhoto, photoFormat, videoFormat, orientation };
 }
 
-// function calculateOrientation(gamma: number, beta: number) {
-//   const radian = Math.atan2(gamma, beta);
-//   const degree = (radian * 180) / Math.PI;
-//   let rotation: Rotation = 'left';
-//   if (degree > -135) rotation = 'top';
-//   if (degree > -45) rotation = 'right';
-//   if (degree > 45) rotation = 'down';
-//   if (degree > 135) rotation = 'left';
-//   // if (Platform.OS === 'android') {
-//   //   rotation = 'right';
-//   //   if (degree > -135) rotation = 'down';
-//   //   if (degree > -45) rotation = 'left';
-//   //   if (degree > 45) rotation = 'top';
-//   //   if (degree > 135) rotation = 'right';
-//   // }
-
-//   console.log('rotation', rotation);
-
-//   const ABSOLUTE_GAMMA = Math.abs(gamma);
-//   const ABSOLUTE_BETA = Math.abs(beta);
-//   const isGammaNegative = Math.sign(gamma) === -1;
-//   let orientation = 0;
-
-//   if (ABSOLUTE_GAMMA <= 0.04 && ABSOLUTE_BETA <= 0.24) {
-//     //Portrait mode, on a flat surface.
-//     orientation = 0;
-//   } else if ((ABSOLUTE_GAMMA <= 1.0 || ABSOLUTE_GAMMA >= 2.3) && ABSOLUTE_BETA >= 0.5) {
-//     //General Portrait mode, accounting for forward and back tilt on the top of the phone.
-//     orientation = 180;
-//   } else {
-//     if (isGammaNegative) {
-//       //Landscape mode with the top of the phone to the left.
-//       orientation = -90;
-//     } else {
-//       //Landscape mode with the top of the phone to the right.
-//       orientation = 90;
-//     }
-//   }
-//   return orientation;
-// }
-
 const reduceRatio = (numerator: number, denominator: number): string => {
   let temp: number | undefined;
   let left: number;
@@ -153,9 +101,7 @@ const reduceRatio = (numerator: number, denominator: number): string => {
 
   if (+numerator < +denominator) {
     temp = numerator;
-    // eslint-disable-next-line no-param-reassign
     numerator = denominator;
-    // eslint-disable-next-line no-param-reassign
     denominator = temp;
   }
 
