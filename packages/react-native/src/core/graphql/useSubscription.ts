79e7ee6b78f3b440c00e4b6329ca03ff685991b4
pchmn <pchmn.dev@gmail.com>
2023-10-13 17:07:59 +0200

    feat(mobile): improve useSubscription for react navigation

--- packages/react-native/src/core/graphql/useSubscription.ts
+++ packages/react-native/src/core/graphql/useSubscription.ts
@@ -1,40 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// eslint-disable-next-line no-restricted-imports
-import {
-  gql,
-  OperationVariables,
-  SubscriptionHookOptions,
-  // eslint-disable-next-line no-restricted-imports
-  useSubscription as useApolloSubscription,
-} from '@apollo/client';
+import { ApolloError, gql, OperationVariables, SubscriptionHookOptions, useApolloClient } from '@apollo/client';
 import { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { DocumentNode } from 'graphql';
 import { useState } from 'react';
+
+import { useFocusEffect } from '../hooks';
 
 function mapResults<I = any, O = any>(data: I) {
   return Object.values(data as any)[0] as O;
 }
 
-export function useSubscription<T, I = any, V extends OperationVariables = OperationVariables>(
+export function useSubscription<T, R = T, I = any, V extends OperationVariables = OperationVariables>(
   mutation: string | DocumentNode | TypedDocumentNode<T, V>,
   options?: SubscriptionHookOptions & {
-    mapFn?: (data: I) => T;
+    mapFn?: (data: I) => R;
   }
 ) {
+  const client = useApolloClient();
+
   const { variables, mapFn = mapResults } = options || {};
-  const [result, setResult] = useState<T>();
 
-  const { data, loading, error } = useApolloSubscription<I>(typeof mutation === 'string' ? gql(mutation) : mutation, {
-    variables,
-  });
+  const [result, setResult] = useState<R>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<ApolloError>();
 
-  if (!loading) {
-    if (data && JSON.stringify(mapFn(data)) !== JSON.stringify(result)) {
-      setResult(mapFn(data));
-    } else if (data === undefined && result !== undefined) {
-      setResult(undefined);
-    }
-  }
+  useFocusEffect(() => {
+    const subscribe = client.subscribe<I>({
+      query: typeof mutation === 'string' ? gql(mutation) : mutation,
+      variables,
+    });
+
+    const subscription = subscribe.subscribe(
+      ({ data }) => {
+        setLoading(false);
+        if ((data === undefined || data === null) && result !== undefined) {
+          setResult(undefined);
+        } else if (data) {
+          const newResult = mapFn(data);
+          if (JSON.stringify(newResult) !== JSON.stringify(result)) {
+            setResult(newResult);
+          }
+        }
+      },
+      (error) => {
+        setError(error);
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
 
   return {
     data: result,
